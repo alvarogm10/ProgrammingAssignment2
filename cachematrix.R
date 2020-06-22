76a63a901dfcaa810b350ae5743deec2fd6abc72
Álvaro <alvarogm10@gmail.com>
2020-06-22 19:01:30 -0400

    First function done

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,22 @@
 ## makeCacheMatrix creates a matrix and save its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinvr <- function(invr) m <<- invr
+	getinvr <- function() m
+	list(set = set, get = get, 
+		setmean = setmean,
+		getmean = getmean)
 }
 
 
-## cacheSolve retrieves the value of the inverse if it was calculated before with makeCacheMatrix
+## cacheSolve retrieves the value of the inverse 
+##if it was calculated before with makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
